2959d313c66a4f28e4a3f7fca1fb320aeba7409c
parthchauhan981104 <48384607+parthchauhan981104@users.noreply.github.com>
2019-06-21 16:46:35 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 from flask import (Flask, jsonify, render_template, request, make_response, url_for)
 from datetime import datetime
 
-
-
-'''import Flask class. An instance of this class will be our WSGI application.'''
+'''An instance of the flask class will be our WSGI application.'''
 
 app = Flask(__name__, template_folder="templates")
 
-'''Next we create an instance of this class. The first argument is
+'''create an instance of this class. The first argument is
 the name of the application’s module or package. If you are using a
 single module (as in this example), you should use __name__ because
 depending on if it’s started as application or imported as module
@@ -18,11 +16,13 @@ static files, and so on.'''
 
 @app.errorhandler(400)
 def not_found(error):
-    return make_response(jsonify( { 'error': 'Bad request' } ), 400)
+    return make_response(jsonify({'error': 'Bad request'}), 400)
+
 
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
+
 
 tasks = [
     {
@@ -59,34 +59,43 @@ def home():
     return render_template('home.html')
 
 
-def make_public_task(task):
+def make_public_task1(task):
     new_task = {}
     for field in task:
         if field == 'id':
-            new_task['uri'] = url_for('get_task', CAK=task['CallingAPIKey'], _external=True) #absolute address
+            new_task['uri'] = url_for('get_task', CAK=task['CallingAPIKey'], _external=True)  # absolute address
         else:
             new_task[field] = task[field]
     return new_task
 
 
-#$ curl -i http://localhost:5000/api/telemetry/task/abcd123
-@app.route('/api/telemetry/task/<string:CAK>', methods=['GET'])
-def get_task(CAK): #with partial search
-    ts=[]
+page_limit = 5
+p = 0
+q = 0
+next_page = False
+
+
+# $ curl -i http://localhost:5000/api/telemetry/task/abcd123
+@app.route('/api/telemetry/task/c/<string:CAK>', methods=['GET'])
+#@app.route('/api/telemetry/task/<string:CAK><int:pno>', methods=['GET'])
+def get_task(CAK):  # with partial search
+    ts = []
     for t in tasks:
-        for i in range(0,len(t['CallingAPIKey'])):
-            if CAK == t['CallingAPIKey'][0:i+1]:
+        for i in range(0, len(t['CallingAPIKey'])):
+            if CAK == t['CallingAPIKey'][0:i + 1]:
                 ts.append(t)
                 break
-    if len(ts):
-        return jsonify({'Task': ts})
-    return jsonify({'Error: ': 'No such task exists. Please specify correct CallingAPIKey.'})
+        return jsonify({'tasks': make_public_task1(task) for task in ts})
+    return jsonify({'Error: ': 'No such task exists. Please specify valid CallingAPIKey.'})
 
 
-#$ curl -i http://localhost:5000/api/telemetry/task
-@app.route('/api/telemetry/task', methods=['GET'])
-def get_alltasks():
-    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
+# $ curl -i http://localhost:5000/api/telemetry/task
+#@app.route('/api/telemetry/task', methods=['GET'])
+@app.route('/api/telemetry/task/<int:pno>', methods=['GET'])
+def get_alltasks(pno):
+    if len(tasks[(pno-1)*page_limit:(pno-1)*page_limit+page_limit]):
+        return jsonify({'tasks': [make_public_task1(task) for task in tasks[(pno-1)*page_limit:(pno-1)*page_limit+page_limit]]})
+    return jsonify({'Error: ': 'No tasks to show.'})
 
 
 # $ curl -i -H "Content-Type: application/json" -X POST -d '{"TicketNo":"aghsg"}' http://localhost:5000/api/telemetry/task
@@ -94,8 +103,8 @@ def get_alltasks():
 @app.route('/api/telemetry/task', methods=['POST'])
 def create_task():
     task = {'id': tasks[-1]['id'] + 1,
-            'TicketNo': request.json.get('TicketNo',""),
-            'DateTimeStamp': request.json.get('DateTimeStamp',""),
+            'TicketNo': request.json.get('TicketNo', ""),
+            'DateTimeStamp': request.json.get('DateTimeStamp', ""),
             'AutomationServiceAccount': request.json.get('AutomationServiceAccount', ""),
             'CallingAPIKey': request.json.get('CallingAPIKey', ""),
             'APIType': request.json.get('APIType', ""),
@@ -111,22 +120,3 @@ def create_task():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
